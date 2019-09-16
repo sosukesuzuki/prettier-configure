778b2154421fe7460116efe73b9783129a773481
Sosuke Suzuki <aosukeke@gmail.com>
2019-09-17 02:07:21 +0900

    Modify to add configuration files

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import fs from 'fs';
+import fs, { write } from 'fs';
 import path from 'path';
 import { promisify } from 'util';
 import execa from 'execa';
@@ -9,8 +9,15 @@ const sparkles = emoji('sparkles');
 
 const readFile = promisify(fs.readFile);
 const writeFile = promisify(fs.writeFile);
+const open = promisify(fs.open);
+const close = promisify(fs.close);
 
-const pkgJsonPath = path.join(process.cwd(), 'package.json');
+const pwd = process.cwd();
+const pkgJsonPath = path.join(pwd, 'package.json');
+
+async function touch(filePath: string): Promise<void> {
+    await close(await open(filePath, 'w'));
+}
 
 async function installToDevDependencies(...packages: string[]) {
     await execa('yarn', ['add', '--dev', ...packages]);
@@ -21,7 +28,7 @@ async function main(): Promise<void> {
     await installToDevDependencies('prettier', 'husky', 'pretty-quick');
     installingSpinar.stop();
 
-    const writingSpinar = ora('Modifying package.json...').start();
+    const pkgJsonSpinar = ora('Modifying package.json...').start();
 
     const pkgJson = JSON.parse(await readFile(pkgJsonPath, 'utf8'));
     pkgJson.scripts = {
@@ -35,7 +42,30 @@ async function main(): Promise<void> {
     const pkgJsonString = JSON.stringify(pkgJson, null, 4);
     await writeFile(pkgJsonPath, pkgJsonString);
 
-    writingSpinar.stop();
+    pkgJsonSpinar.stop();
+
+    const configFilesSpinar = ora('Adding config for Prettier').start();
+
+    const prettierrcPath = path.join(pwd, '.prettierrc.yaml');
+    await touch(prettierrcPath);
+    await writeFile(
+        prettierrcPath,
+        `trailingComma: 'all'
+singleQuote: true
+tabWidth: 4
+`,
+    );
+
+    const prettierIgnorePath = path.join(pwd, '.prettierignore');
+    await touch(prettierIgnorePath);
+    await writeFile(
+        prettierIgnorePath,
+        `/node_modules
+package.json
+`,
+    );
+
+    configFilesSpinar.stop();
 
     console.log(sparkles, ' Done');
 }
